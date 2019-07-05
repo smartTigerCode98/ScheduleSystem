12927478088f2315e449169f19b7797b778b4548
Yuriy Danko <danko98uran@gmail.com>
2019-07-05 10:28:28 +0300

    Added new method

--- DataSourceAbstractions/Contracts/IRepository.cs
+++ DataSourceAbstractions/Contracts/IRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DataSourceAbstractions.Contracts
@@ -5,6 +6,7 @@ namespace DataSourceAbstractions.Contracts
     public interface IRepository<T>
     {
         Task<T> FindById(int id);
+        Task<IList<T>> FindAll();
         Task Create(T entities);
         Task Update(T entities);
         Task Delete(T entities);
